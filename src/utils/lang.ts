f2af0b85d62e5f8418e4aaea366bd1d1f9c0270b
Misaka <447f.misaka@outlook.com>
2019-02-09 02:50:54 +0800

    Revert unstable features "CherryPick: Make Watchable Trackable"
    This reverts commit a7911ce5

--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -37,48 +37,37 @@ export const xr = (a: any[], i: number, e: any) => {
 
 export class Watchable {
   private storage: object = {};
-  private triggers: Array<(T: string) => void> = [];
+  private triggers: Array<() => void> = [];
 
   public init<T>(obj: T) {
     this.storage = obj as unknown as object;
     return new Proxy(this.storage, {
-      get: (target: object, property: symbol) => {
+      get: (target: object, property: string) => {
         // // strict mode
         // if (!target.hasOwnProperty(property)) {
         //   console.log(property);
         //   throw new Error(`Refused to write: invalid target ${property}`);
         // }
-        if (property.toString() === "__isInstanceOf_Watchable") {
-          return true;
-        }
         // @ts-ignore since just want to simulate original getter.
         return obj[property];
       },
       // @ts-ignore
-      set: (target: object, property: symbol, value: any) => {
+      set: (target: object, property: string, value: any) => {
         // if (!target.hasOwnProperty(property)) {
         //   console.log(property);
         //   throw new Error(`Refused to write: invalid target ${property}`);
         // }
-        // // @ts-ignore
-        // console.log(target[property]);
-        // console.log(value);
-        // @ts-ignore
-        if (target[property].__isInstanceOf_Watchable && !value.__isInstanceOf_Watchable) {
-          throw new Error("Tries to overwrite a proxy pointer " +
-            "with a pointer that is not pointing at a proxy.");
-        }
         // @ts-ignore
         obj[property] = value;
         for (const i of this.triggers) {
-          i(property.toString());
+          i();
         }
         return true;
       }
     }) as unknown as T;
   }
 
-  public registerTrigger(listener: (T: string) => void) {
+  public registerTrigger(listener: () => void) {
     this.triggers.push(listener);
   }
 }
