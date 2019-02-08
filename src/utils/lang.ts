a7911ce5e419659237471dd478293e430ee4566d
Misaka <447f.misaka@outlook.com>
2019-02-09 02:15:55 +0800

    CherryPick: Make Watchable Trackable

--- src/utils/lang.ts
+++ src/utils/lang.ts
@@ -37,37 +37,48 @@ export const xr = (a: any[], i: number, e: any) => {
 
 export class Watchable {
   private storage: object = {};
-  private triggers: Array<() => void> = [];
+  private triggers: Array<(T: string) => void> = [];
 
   public init<T>(obj: T) {
     this.storage = obj as unknown as object;
     return new Proxy(this.storage, {
-      get: (target: object, property: string) => {
+      get: (target: object, property: symbol) => {
         // // strict mode
         // if (!target.hasOwnProperty(property)) {
         //   console.log(property);
         //   throw new Error(`Refused to write: invalid target ${property}`);
         // }
+        if (property.toString() === "__isInstanceOf_Watchable") {
+          return true;
+        }
         // @ts-ignore since just want to simulate original getter.
         return obj[property];
       },
       // @ts-ignore
-      set: (target: object, property: string, value: any) => {
+      set: (target: object, property: symbol, value: any) => {
         // if (!target.hasOwnProperty(property)) {
         //   console.log(property);
         //   throw new Error(`Refused to write: invalid target ${property}`);
         // }
+        // // @ts-ignore
+        // console.log(target[property]);
+        // console.log(value);
+        // @ts-ignore
+        if (target[property].__isInstanceOf_Watchable && !value.__isInstanceOf_Watchable) {
+          throw new Error("Tries to overwrite a proxy pointer " +
+            "with a pointer that is not pointing at a proxy.");
+        }
         // @ts-ignore
         obj[property] = value;
         for (const i of this.triggers) {
-          i();
+          i(property.toString());
         }
         return true;
       }
     }) as unknown as T;
   }
 
-  public registerTrigger(listener: () => void) {
+  public registerTrigger(listener: (T: string) => void) {
     this.triggers.push(listener);
   }
 }
