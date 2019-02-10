381bf6265493fe22034048193503caff02103ae3
Misaka <447f.misaka@outlook.com>
2019-02-10 15:21:11 +0800

    websocket server init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,1 +1,13 @@
-console.log("Hi");
+import WebSocket from "ws";
+
+const wss = new WebSocket.Server({ port: 52504 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function incoming(message) {
+    console.log("received: %s", message);
+  });
+
+  ws.send("Hello");
+});
+
+console.log("Websocket server started");
