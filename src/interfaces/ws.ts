4849e7374368b7b6ba086b20780fb60ec43e105b
Misaka <447f.misaka@outlook.com>
2019-02-13 17:18:39 +0800

    env config && ci config

--- src/interfaces/ws.ts
+++ src/interfaces/ws.ts
@@ -1,6 +1,10 @@
-const env = process.env.NODE_ENV;
+const env = process.env.NODE_ENV as string;
 console.log(`ENV: ${env}`);
 console.log(`${env === "development" ? ">" : " "} localhost`);
-console.log(`${env === "production" ? ">" : " "} api.trpg.misaka.org`);
+console.log(`${env === "production" ? ">" : " "} trpg-api.azurewebsites.net`);
 
-export const serverAddr = `ws://${window.location.hostname}:52504/`;
+// @ts-ignore
+export const serverAddr = {
+  development: `ws://${window.location.hostname}:52504/`,
+  production: `ws://trpg-api.azurewebsites.net:52504/`
+}[env];
