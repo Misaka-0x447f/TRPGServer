ba8b80eda79b110df639b9ac691d1e0d2919bff3
Misaka <447f.misaka@outlook.com>
2019-02-13 16:36:17 +0800

    env test

--- src/interfaces/ws.ts
+++ src/interfaces/ws.ts
@@ -1,1 +1,6 @@
+const env = process.env.NODE_ENV;
+console.log(`ENV: ${env}`);
+console.log(`${env === "development" ? ">" : " "} localhost`);
+console.log(`${env === "production" ? ">" : " "} api.trpg.misaka.org`);
+
 export const serverAddr = `ws://${window.location.hostname}:52504/`;
